0fd45447c9994cd288c4fde879ac0b9a7c582a73
etleyden <etleyden@gmail.com>
2024-10-22 15:38:54 -0400

    initial commit

---
+++ remove_labels.py
@@ -0,0 +1,52 @@
+import sys
+import subprocess
+import pandas as pd
+
+if len(sys.argv) < 1:
+    print("Missing sequences filepath. Exiting...")
+    sys.exit()
+
+seq_fpath = sys.argv[1]
+
+def remove_chars(string, chars_to_remove):
+    translation_table = str.maketrans("", "", chars_to_remove)
+    return string.translate(translation_table)
+
+def main():
+    labels = []
+    with open(seq_fpath) as f:
+        lines = f.readlines()
+        print(f"{len(lines)} lines in the input.")
+        count = 0
+        NNAA = "BJOUZX"
+        with open("proteins.txt", "w+") as p:
+            for line in lines:
+                line = line.split(",")
+                prot = line[0]
+                badSeq = False
+                for i in prot:
+                    if i in NNAA:
+                        count += 1
+                        badSeq = True
+                        break
+                if badSeq:
+                    prot = remove_chars(prot, NNAA)
+                p.write(f"{prot}\n")
+                labels.append(line[1])
+
+    
+    result = subprocess.run([f"python pfeature_comp/src/pfeature_comp.py -i proteins.txt -o features.txt -j AAC"])
+
+    if result.returncode == 0:
+        features = pd.read_csv("features.txt")
+        features["Labels"] = labels
+        features.to_csv("features.txt")
+    else:
+        print("There was an error while using pfeature to compute features")
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
+    sys.exit()
+    
+        
