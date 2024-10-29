1038a794f113afa9b4fcdb21fea864b686c3a792
Ethan Leyden <leydene@vcu.edu>
2024-10-28 20:45:55 -0400

    Added print statements

--- remove_labels.py
+++ remove_labels.py
@@ -16,18 +16,18 @@ def remove_chars(string, chars_to_remove):
 def normalize_columns(df, columns):
     # Copy the dataframe to avoid modifying the original one
     df_normalized = df.copy()
-    
+
     # Apply min-max normalization to each specified column
     for column in columns:
         min_value = df_normalized[column].min()
         max_value = df_normalized[column].max()
-        
+
         # Avoid division by zero
         if max_value - min_value != 0:
             df_normalized[column] = (df_normalized[column] - min_value) / (max_value - min_value)
         else:
             df_normalized[column] = 0  # If all values are the same, normalize to 0
-    
+
     return df_normalized
 def main():
     labels = []
@@ -51,21 +51,23 @@ def main():
                 p.write(f"{prot}\n")
                 labels.append(line[1].strip())
 
-    
+
     # list of features and array of commands
     features = ["AAC", "PCP"]
     commands = [f"python pfeature_comp/src/pfeature_comp.py -i proteins.txt -o {feature}_features.csv -j {feature}" for feature in features]
 
     # run the commands in their own processes
     pids = []
-    for command in commands:
+    for idx, command in enumerate(commands):
         pid = os.fork()
         if pid == 0:
-            if not os.path.exists(f"{feature}_features.csv"):
+            if not os.path.exists(f"{features[idx]}_features.csv"):
+                print(f"Computing {features[idx]}...")
                 result = subprocess.run(command, shell=True)
+                print(f"Finished computing {features[idx]}")
                 os._exit(result.returncode)
             else:
-                print(f"Features {feature} already computed. Skipping.")
+                print(f"Features {features[idx]} already computed. Skipping.")
                 os._exit(0)
         else:
             pids.append(pid)
@@ -93,5 +95,5 @@ def main():
 if __name__ == "__main__":
     main()
     sys.exit()
-    
-        
+
+
